6eb552845ab13e1928dd98bf7890733b89c34b0e
Ritu Bafna <ritujane78@gmail.com>
2025-09-19 16:54:29 +0545

    add AWS deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,7 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec')
 function run() {
-    core.notice('Hello from my custom JavaScript Actioan.');
+    const bucket = core.getInput('bucket', {required: true})
+    const region = core.getInput('region', {required: true})
+    const distFolder = core.getInput('dist-folder', {required: true})
+
+    const s3URI = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${region}`)
+
+    core.notice('Hello from my custom JavaScript Action.');
 
 }
 
